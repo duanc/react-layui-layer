abc78e1f1623da54dbff36b324dce33eea7d07b0
duanc <duan22334@163.com>
2019-04-02 13:23:56 +0800

    添加layer

--- layer/LayerEle.jsx
+++ layer/LayerEle.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import layer from 'react';
 
 export default class Layer extends Component {
 
     componentDidMount() {
-        console.log('執行了Layer')
+        console.log('執行了Layer');
+        console.log(layer)
     }
 
   render() {
