e9b28b4eaa0d5dec7d271a170cd5ac4a0de4db80
duanc <duan22334@163.com>
2019-04-02 10:20:53 +0800

    添加componentDidMount

--- layer/LayerEle.js
+++ layer/LayerEle.js
@@ -7,6 +7,11 @@ import React, { Component } from 'react';
  */
  class Layer extends Component {
 
+
+    componentDidMount() {
+        console.log('執行了Layer')
+    }
+
   render() {
     return (
       <div style={{color:'#0ff'}}>
